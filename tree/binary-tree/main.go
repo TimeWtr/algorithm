a591607866b6aea8fa952a073f0c4942bd644c9b
2301_79425991 <2301_79425991@noreply.gitcode.com>
2025-08-18 14:59:57 +0800

    feat: 添加路径和的计算

--- tree/binary-tree/main.go
+++ tree/binary-tree/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 type BinaryTree struct {
-	// 节点值
+	// 当前节点的值
 	Val int
 	// 左子树
 	Left *BinaryTree
@@ -11,33 +11,27 @@ type BinaryTree struct {
 	Right *BinaryTree
 }
 
-// 构造二叉搜索树
+// 构建二叉搜索树
 func buildBST(cur []int, lowIdx, highIdx int) *BinaryTree {
-	if cur == nil {
+	if len(cur) == 0 || lowIdx > highIdx {
 		return nil
 	}
 
-	if lowIdx > highIdx {
-		return nil
-	}
-
-	var midIdx int
-	midIdx = lowIdx + (highIdx-lowIdx)/2
-	root := &BinaryTree{}
-	root.Left = buildBST(cur, lowIdx, midIdx-1)
-	root.Right = buildBST(cur, midIdx+1, highIdx)
-	root.Val = cur[midIdx]
-	return root
+	bst := new(BinaryTree)
+	midIdx := lowIdx + (highIdx-lowIdx)/2
+	bst.Val = cur[midIdx]
+	bst.Left = buildBST(cur, lowIdx, midIdx-1)
+	bst.Right = buildBST(cur, midIdx+1, highIdx)
+	return bst
 }
 
-func main() {
-	cur := []int{1, 2, 3, 4, 5, 6, 7}
-	root := buildBST(cur, 0, len(cur)-1)
-	fmt.Println("BST: ", root)
+func levelTraversal(tree *BinaryTree) []int {
+	if tree == nil {
+		return nil
+	}
 
-	// 层序遍历打印所有结果
 	var res []int
-	queue := []*BinaryTree{root}
+	queue := []*BinaryTree{tree}
 	for len(queue) > 0 {
 		queueSize := len(queue)
 		for i := 0; i < queueSize; i++ {
@@ -50,7 +44,87 @@ func main() {
 				queue = append(queue, queue[i].Right)
 			}
 		}
+
 		queue = queue[queueSize:]
 	}
-	fmt.Println("遍历结果：", res)
+
+	return res
+}
+
+// 计算路径和(递推思想实现)
+func calculateSums(root *BinaryTree) []int {
+	if root == nil {
+		return nil
+	}
+
+	var results []int
+	nodeQ := []*BinaryTree{root}
+	sumsQ := []int{0}
+	for len(nodeQ) > 0 {
+		curNode := nodeQ[0]
+		nodeQ = nodeQ[1:]
+		curSum := sumsQ[0] + curNode.Val
+		sumsQ = sumsQ[1:]
+		if curNode.Left == nil && curNode.Right == nil {
+			// 叶子节点
+			results = append(results, curSum)
+			continue
+		}
+
+		if curNode.Left != nil {
+			nodeQ = append(nodeQ, curNode.Left)
+			sumsQ = append(sumsQ, curSum)
+		}
+
+		if curNode.Right != nil {
+			nodeQ = append(nodeQ, curNode.Right)
+			sumsQ = append(sumsQ, curSum)
+		}
+	}
+
+	return results
+}
+
+// 计算路径和(递归思想实现)
+func calculateSumsRecursion(root *BinaryTree) []int {
+	if root == nil {
+		return nil
+	}
+
+	var sums []int
+	var dfs func(node *BinaryTree, sum int)
+	dfs = func(node *BinaryTree, sum int) {
+		if node == nil {
+			return
+		}
+
+		curSum := sum + node.Val
+		if node.Left == nil && node.Right == nil {
+			sums = append(sums, curSum)
+			return
+		}
+
+		if node.Left != nil {
+			dfs(node.Left, curSum)
+		}
+
+		if node.Right != nil {
+			dfs(node.Right, curSum)
+		}
+	}
+
+	dfs(root, 0)
+	return sums
+}
+
+func main() {
+	cur := []int{1, 2, 3, 4, 5, 6, 7}
+	bst := buildBST(cur, 0, len(cur)-1)
+	fmt.Println("构建二叉搜索树：", bst)
+	res := levelTraversal(bst)
+	fmt.Println("层级遍历二叉搜索树：", res)
+	sums := calculateSums(bst)
+	fmt.Println("路径和(递推)：", sums)
+	sums1 := calculateSumsRecursion(bst)
+	fmt.Println("路径和(递归)：", sums1)
 }
